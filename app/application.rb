9d426977bbdb009fd2276a4a3ff9f1b5c033f804
Joseph Kao <josephtkao@gmail.com>
2020-06-27 17:18:06 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,16 @@
 class application
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack:Request.new(env)
 
+    if req.path.match(/items/)
+      resp.write "You requested items"
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+
+    resp.finish
+  end
 
 end
