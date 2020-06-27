561cd3f3f9408c45a4d235dec6d2d5925155104e
Joseph Kao <josephtkao@gmail.com>
2020-06-27 17:21:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,13 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      search = req.params
+      search = req.path.split("items/").last
+      if @@items.include?(search)
+        resp.write "#{search.price}"
+      else
+        resp.write "Item not found"
+      end
+        
     else
       resp.write "Route not found"
       resp.status = 404
